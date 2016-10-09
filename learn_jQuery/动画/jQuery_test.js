8a32f6a69420bfa5e3b30b851357d9fe3a240cf3
CalvinoShaw <xiaobingyang_007@126.com>
2016-10-09 23:57:49 +0800

    begin jQuery Demo

---
+++ learn_jQuery/动画/jQuery_test.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+	// $("tbody").hide(700);
+	// $("tbody").show(1000);
+	
+	// $("tbody").fadeOut(1000);
+	// $("tbody").fadeIn(1000);
+
+	$(".testing-slide").slideUp(1000);
+	$(".testing-slide").slideDown(500);
+
+	$(".testing-fade").fadeOut(1000);
+	$(".testing-fade").fadeIn(1000);
+
+	$(".testing").hide(700);
+	$(".testing").show(1000);
+});
